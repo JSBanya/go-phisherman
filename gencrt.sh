9019a73c2ce892401a296f2e3234220fbbd478ff
Brandon Botsch <brandon.botsch@gmail.com>
2018-04-29 16:04:41 -0400

    Use same SUBJ env variable in init_rootca.sh like gencrt.sh

--- gencrt.sh
+++ gencrt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f certs/$1.crt ]; then
-	SUBJ="/C=US/ST=New Jersey/O=PhisherMan/CN=$1"
+	SUBJ="/C=US/ST=New Jersey/L=Hoboken/O=PhisherMan/OU=Proxy Server/CN=$1"
 	export SAN="DNS:$1"
 	openssl req -new -nodes -newkey rsa:2048 -keyout certs/$1.key -out certs/$1.csr -subj "$SUBJ" -config openssl.cnf -reqexts SAN
 	openssl x509 -req -in certs/$1.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out certs/$1.crt -extensions SAN -extfile openssl_san.cnf
--- init_rootca.sh
+++ init_rootca.sh
@@ -3,7 +3,7 @@ if [ -f rootCA.crt ]; then
 	echo "Root CA certificate has already been generated! Aborting."
 	exit
 fi
-(echo "US"; echo "New Jersey"; echo "Hoboken"; echo "PhisherMan"; echo "Proxy Server"; echo "PhisherMan Root CA"; echo) | \
-openssl req -x509 -new -nodes -newkey rsa:2048 -keyout rootCA.key -out rootCA.crt 2> /dev/null && \
+SUBJ="/C=US/ST=New Jersey/L=Hoboken/O=PhisherMan/OU=Proxy Server/CN=PhisherMan Root CA"
+openssl req -x509 -new -nodes -newkey rsa:2048 -keyout rootCA.key -out rootCA.crt -subj "$SUBJ" && \
 echo "Root CA certificate has been generated" && \
 echo "Please install rootCA.crt in your browser before using PhisherMan"
